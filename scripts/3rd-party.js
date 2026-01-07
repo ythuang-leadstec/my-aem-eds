75160b0ebcb2822329041cd38e207592c2c04040
leadstec-ythuang <26317078+ythuang-leadstec@users.noreply.github.com>
2026-01-07 23:13:51 +0800

    update

--- scripts/3rd-party.js
+++ scripts/3rd-party.js
@@ -1,12 +1,11 @@
-import { loadCSS } from './aem.js';
+import { loadCSS } from "./aem.js";
 
 /**
  * 通用 Swiper 加载器
  * @returns {Promise<Class>} Swiper 类
  */
 export async function loadSwiper() {
-  loadCSS(`${window.hlx.codeBasePath}/scripts/libs/swiper-bundle.min.css`);
-  const mod = await import('./libs/swiper.esm.browser.bundle.min.js');
+  loadCSS(`${window.hlx.codeBasePath}/scripts/libs/swiper/swiper.min.css`);
+  const mod = await import("./libs/swiper/swiper.esm.browser.bundle.min.js");
   return mod.default;
 }
-
