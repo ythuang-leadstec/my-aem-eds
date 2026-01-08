5c6214e413375adf9f3f6cdff2d5240af5edf32d
leadstec-ythuang <26317078+ythuang-leadstec@users.noreply.github.com>
2026-01-08 23:51:35 +0800

    add support tools component

---
+++ blocks/support-tools/support-tools.js
@@ -0,0 +1,7 @@
+import { html, render } from "https://esm.sh/lit-html";
+import { loadSwiper } from "../../scripts/3rd-party.js";
+import { moveInstrumentation } from "../../scripts/scripts.js";
+
+const COMPONENT_CLASS = "cmp__support-tools";
+
+export default async function decorate(block) {}
