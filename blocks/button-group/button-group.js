9d42746b16266a2c1e34ec289437f4a7bdcbc2f1
leadstec-ythuang <26317078+ythuang-leadstec@users.noreply.github.com>
2026-01-23 00:44:33 +0800

    update

--- blocks/button-group/button-group.js
+++ blocks/button-group/button-group.js
@@ -26,23 +26,25 @@ function getButtonGroupConfig(block) {
 function parseItemConfig(row) {
   const cols = [...row.children];
   const [
-    btnStyle,     // 0: Button Style
-    btnLabelCol,    // 1: Button Label
-    btnLinkCol,     // 2: Button Link
-    btnIconCol,    // 3: Button Icon
-    btnHoverIconCol,     // 4: Button Hover Icon
+    btnStyle, // 0: Button Style
+    btnLabelCol, // 1: Button Label
+    btnLinkCol, // 2: Button Link
+    btnIconCol, // 3: Button Icon
+    btnHoverIconCol, // 4: Button Hover Icon
   ] = cols;
   let hoverIconEl = null;
   let iconEl = null;
   if (btnIconCol) {
     iconEl = btnIconCol.querySelector("picture");
-    if (iconEl) iconEl.querySelector("img").classList.add("icon");
+    if (iconEl) iconEl.classList.add("icon");
   }
   if (btnHoverIconCol) {
     hoverIconEl = btnHoverIconCol.querySelector("picture");
-    if (hoverIconEl) hoverIconEl.querySelector("img").classList.add("hover-icon");
+    if (hoverIconEl)
+      hoverIconEl.classList.add("hover-icon");
   }
   return {
+    source: row,
     style: btnStyle?.textContent?.trim().toLowerCase() || "primary-m",
     label: btnLabelCol?.textContent?.trim() || "",
     link: btnLinkCol?.textContent?.trim() || "#",
@@ -61,19 +63,25 @@ function setButtonTemplate(config) {
   let buttonHtml;
   if (config.style === "icon") {
     buttonHtml = html`
-      <a class="btn-icon btn-icon--primary ${config.hoverIcon ? "is-have-hover-icon" : ""}" href="${config.link}"> ${config.icon} ${config.hoverIcon}</a>
+      <a
+        class="btn btn-icon btn-icon--primary ${config.hoverIcon
+        ? "is-have-hover-icon"
+        : ""}"
+        href="${config.link}"
+      >${config.icon}${config.hoverIcon}</a>
     `;
   } else if (config.style === "text-icon") {
     buttonHtml = html`
-      <a class="btn-text-icon btn-text-icon--primary" href="${config.link}">
-        ${config.icon}
-        ${config.hoverIcon}
-        <span class="body-1" data-inst="label">${config.label}</span>
+      <a class="btn btn-text-icon btn-text-icon--primary ${config.hoverIcon ? "is-have-hover-icon" : ""}" href="${config.link}">
+        ${config.icon}${config.hoverIcon}<span class="body-1" data-inst="label">${config.label}</span>
       </a>
     `;
   } else {
     buttonHtml = html`
-      <a class="btn btn--${config.style} body-1" href="${config.link}" data-inst="label"
+      <a
+        class="btn btn--${config.style} body-1"
+        href="${config.link}"
+        data-inst="label"
         >${config.label}</a
       >
     `;
@@ -90,9 +98,12 @@ export default function decorate(block) {
 
   console.log(items);
 
-
   const template = html`
-    <div class="${COMPONENT_CLASS} ${items.length > 1 ? `${COMPONENT_CLASS}--multiple-buttons` : ""}">
+    <div
+      class="${COMPONENT_CLASS} ${items.length > 1
+      ? `${COMPONENT_CLASS}--multiple-buttons`
+      : ""}"
+    >
       <div class="${COMPONENT_CLASS}__container component-layout">
         <div class="${COMPONENT_CLASS}__wrapper is-align-${config.align}">
           ${items.map((item) => setButtonTemplate(item))}
@@ -105,4 +116,10 @@ export default function decorate(block) {
   block.classList.add(COMPONENT_CLASS);
 
   render(template, block);
+  const btnItems = block.querySelectorAll(".btn");
+  items.forEach((item, index) => {
+    console.log(item.source);
+    console.log(btnItems[index]);
+    moveInstrumentation(item.source, btnItems[index]);
+  });
 }
